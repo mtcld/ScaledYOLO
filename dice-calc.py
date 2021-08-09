4e8a113909529bb0af133311788c1d515378de43
gaurav67890 <gauravsharma01091993@gmail.com>
2021-08-10 06:46:21 +0900

    dice calc

--- dice-calc.py
+++ dice-calc.py
@@ -171,6 +171,7 @@ for i in imgs_path:
     #pred=pred/255
     #cv2.imwrite(str(n)+'.png',pred)
     mask=mask.copy()[int(0.025*mask.shape[0]):int(0.975*mask.shape[0]),int(0.025*mask.shape[1]):int(0.975*(mask.shape[1]))]
+    cv2.imwrite('maskextra/'+str(n)+'.png',mask)
     pred=pred.copy()[int(0.025*pred.shape[0]):int(0.975*pred.shape[0]),int(0.025*pred.shape[1]):int(0.975*(pred.shape[1]))]
     print(mask.shape,pred.shape)
     print(np.unique(mask),np.unique(pred))
