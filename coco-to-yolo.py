c987e4cd3a2800f39a9de6648a0eae9880dfcd88
thangnx183 <nxthang881972@gmail.com>
2021-08-30 14:51:46 +0700

    add condition for convert

--- coco-to-yolo.py
+++ coco-to-yolo.py
@@ -2,15 +2,15 @@ import json
 import cv2
 from tqdm import tqdm
 from pathlib import Path
-damage_name='dent'
-mode=['train','test','valid']
-annt_dir='/mmdetection/data/dent_latest2/annotations'
-img_dir='/mmdetection/data/dent_latest2/images'
-output_dir='data2'
+#damage_name='dent'
+mode=['merimen_bbox']
+annt_dir='/workspace/share/ai-damage-detection/damage_detection/augmentation/merimen_coco/26_8/dent/annotations'
+img_dir='/workspace/share/ai-damage-detection/damage_detection/augmentation/merimen_coco/26_8/dent/images'
+output_dir='dent_yolo'
 
 for m in mode:
-    Path("data2/"+m).mkdir(parents=True, exist_ok=True)
-    with open(annt_dir+'/'+damage_name+'_'+m+'.json') as f:
+    Path(output_dir+"/"+m).mkdir(parents=True, exist_ok=True)
+    with open(annt_dir+'/'+m+'.json') as f:
         data=json.load(f)
     for i in tqdm(range(len(data['images']))):
         image_id=  data['images'][i]['id']
@@ -20,6 +20,7 @@ for m in mode:
         #print(img_out_path)
         cv2.imwrite(img_out_path,img)
         with open(output_dir+'/'+m+'.txt','a') as file_text:
+            #img_out_path = 'merge_crack_yolo/valid_mask/'+fn
             file_text.write(img_out_path+'\n')
         fn_text=fn[:fn.rfind('.')]+'.txt'
         height=data['images'][i]['height']
@@ -28,12 +29,14 @@ for m in mode:
             if data['annotations'][j]['image_id']==image_id:
                 bbox=data['annotations'][j]['bbox']
 
-                x_c= str((bbox[0]+0.5*bbox[2])/width)
-                y_c = str((bbox[1] + 0.5 * bbox[3]) / height)
-                x_w = str(bbox[2] / width)
-                y_h = str(bbox[3] / height)
+                x_c= (bbox[0]+0.5*bbox[2])/width
+                y_c = (bbox[1] + 0.5 * bbox[3]) / height
+                x_w = bbox[2] / width
+                y_h = bbox[3] / height
 
+                if x_c >=1 or y_c >= 1 or x_w >= 1 or y_h >= 1 :
+                    continue
 
                 with open(output_dir+'/'+m+'/'+fn_text, 'a') as the_file:
-                    the_file.write('0 '+x_c+' '+y_c+' '+x_w+' '+y_h+'\n')
+                    the_file.write('0 '+str(x_c)+' '+str(y_c)+' '+str(x_w)+' '+str(y_h)+'\n')
 
