a6a54f01f03217c6e857a25e697bcb82e92ee7b9
thangnx183 <nxthang881972@gmail.com>
2021-10-07 09:07:43 +0000

    integrate tensorRT inference

--- export_onnx.py
+++ export_onnx.py
@@ -8,12 +8,54 @@ from models.experimental import attempt_load
 from utils.activations import Mish
 from onnxsim import simplify
 
+import tensorrt as trt
+
+def save_trt_engine(engine, path):
+    """Serialize TensorRT engine to disk.
+    Arguments:
+        engine (tensorrt.ICudaEngine): TensorRT engine to serialize
+        path (str): disk path to write the engine
+    """
+    with open(path, mode='wb') as f:
+        f.write(bytearray(engine.serialize()))
+
+# build tensorRT engine from onnx_file_path and save at engine_file_path
+def build_engine(onnx_file_path,engine_file_path):
+    # initialize TensorRT engine and parse ONNX model
+    builder = trt.Builder(TRT_LOGGER)
+    EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
+    network = builder.create_network(EXPLICIT_BATCH)
+    parser = trt.OnnxParser(network, TRT_LOGGER)
+
+    # allow TensorRT to use up to 1GB of GPU memory for tactic selection
+    builder.max_workspace_size = 1 << 30
+    # we have only one image in batch
+    builder.max_batch_size = 1
+    # use FP16 mode if possible
+    if builder.platform_has_fast_fp16:
+        builder.fp16_mode = True
+        print('use FP16 mode inference !!')
+
+    # parse ONNX
+    with open(onnx_file_path, 'rb') as model:
+        print('Beginning ONNX file parsing')
+        parser.parse(model.read())
+    print('Completed parsing of ONNX file')
+
+    # generate TensorRT engine optimized for the target platform
+    print('Building an engine...')
+    engine = builder.build_cuda_engine(network)
+    print("Completed creating Engine")
+    print('Saving engine ...')
+    save_trt_engine(engine,engine_file_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='./weights/yolov4-p5.pt', help='weights path')  # from yolov5/models/
-    parser.add_argument('--img-size', nargs='+', type=int, default=[896, 896], help='image size')  # height, width
+    parser.add_argument('--img-size', nargs='+', type=int, default=[1024, 1024], help='image size')  # height, width
     parser.add_argument('--batch-size', type=int, default=1, help='batch size')
+    parser.add_argument('--engine_file',type=str, default=None, help='output engine file path')
+    parser.add_argument('--model_data_file',type=str, default=None, help='output model data (grid,stride,...) file path')
     opt = parser.parse_args()
     opt.img_size *= 2 if len(opt.img_size) == 1 else 1  # expand
     print(opt)
@@ -25,6 +67,7 @@ if __name__ == '__main__':
     model = attempt_load(opt.weights, map_location=torch.device('cpu'))  # load FP32 model
 
     # Update model
+    
     for k, m in model.named_modules():
         m._non_persistent_buffers_set = set()  # pytorch 1.6.0 compatability
         if isinstance(m, models.common.Conv) and isinstance(m.act, models.common.Mish):
@@ -39,10 +82,23 @@ if __name__ == '__main__':
                 m.bn = bn
             if isinstance(m.act, models.common.Mish):
                 m.act = Mish()  # assign activation
+    
         # if isinstance(m, models.yolo.Detect):
         #     m.forward = m.forward_export  # assign forward (optional)
 
     model.eval()
+
+    # Save spec of model
+    detector = model.model[-1]
+    db = {
+        'num_detector_layer': detector.nl,
+        'grid': detector.grid,
+        'anchor_grid': detector.anchor_grid,
+        'stride' : detector.stride
+    }
+    print('Saving spec of model into ',opt.model_data_file,'....')
+    torch.save(bd,opt.model_data_file)
+
     model.model[-1].export = True  # set Detect() layer export=True
     # y = model(img)  # dry run
 
@@ -62,8 +118,13 @@ if __name__ == '__main__':
         onnx.save(model_simp, f)
         # print(onnx.helper.printable_graph(onnx_model.graph))  # print a human readable model
         print('ONNX export success, saved as %s' % f)
+
     except Exception as e:
         print('ONNX export failure: %s' % e)
 
-    # Finish
-    print('\nExport complete. Visualize with https://github.com/lutzroeder/netron.')
+    # tensorRT build engine and save in .trt file
+    try :
+        build_engine(f,opt.engine_file)
+    except Exception as e:
+        print('TensorRT export failure: %s' % e)
+
--- trt_inference.py
+++ trt_inference.py
@@ -4,10 +4,10 @@ import pycuda.autoinit
 import numpy as np
 import tensorrt as trt
 import cv2
-
-ONNX_FILE_PATH = "checkpoints/scratch/scratch_23_9.onnx"
-# logger to capture errors, warnings, and other information during the build and inference phases
-TRT_LOGGER = trt.Logger()
+from utils.datasets import LoadImagesBatch
+import math
+from collections import OrderedDict
+from utils.general import (check_img_size, non_max_suppression, scale_coords)
 
 class HostDeviceMem(object):
     def __init__(self, host_mem, device_mem):
@@ -20,118 +20,164 @@ class HostDeviceMem(object):
     def __repr__(self):
         return self.__str__()
 
-def save_trt_engine(engine, path):
-    """Serialize TensorRT engine to disk.
-    Arguments:
-        engine (tensorrt.ICudaEngine): TensorRT engine to serialize
-        path (str): disk path to write the engine
-    """
-    with open(path, mode='wb') as f:
-        f.write(bytearray(engine.serialize()))
+class TensorRT_model():
+    def __init__(self,engine_file_path,model_data_file_path,confident_score,iou_threshold,label):
+        self.engine, self.context = self.load_trt_engine(engine_file_path)
+        db = torch.load(model_data_file_path)
+        self.num_detector_layer = db['num_detector_layer']
+        self.grid = db['grid']
+        self.anchor_grid = db['anchor_grid']
+        self.stride = db['stride']
+        self.no = 6 # number of class + 5 = number of output per anchor
 
-def load_trt_engine(path):
-    """Deserialize TensorRT engine from disk.
-    Arguments:
-        path (str): disk path to read the engine
-    Returns:
-        tensorrt.ICudaEngine: the TensorRT engine loaded from disk
-    """
-    with trt.Logger() as logger, trt.Runtime(logger) as runtime:
-        with open(path, mode='rb') as f:
-            engine_bytes = f.read()
-        engine = runtime.deserialize_cuda_engine(engine_bytes)
-        return engine, engine.create_execution_context()
+        self.conf_score = confident_score
+        self.iou_thres = iou_threshold
+        self.label = label
 
-def build_engine(onnx_file_path):
-    # initialize TensorRT engine and parse ONNX model
-    builder = trt.Builder(TRT_LOGGER)
-    EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
-    network = builder.create_network(EXPLICIT_BATCH)
-    parser = trt.OnnxParser(network, TRT_LOGGER)
+    def load_trt_engine(self,path):
+        """Deserialize TensorRT engine from disk.
+        Arguments:
+            path (str): disk path to read the engine
+        Returns:
+            tensorrt.ICudaEngine: the TensorRT engine loaded from disk
+        """
+        with trt.Logger() as logger, trt.Runtime(logger) as runtime:
+            with open(path, mode='rb') as f:
+                engine_bytes = f.read()
+            engine = runtime.deserialize_cuda_engine(engine_bytes)
+            return engine, engine.create_execution_context()
 
-    # allow TensorRT to use up to 1GB of GPU memory for tactic selection
-    builder.max_workspace_size = 1 << 30
-    # we have only one image in batch
-    builder.max_batch_size = 1
-    # use FP16 mode if possible
-    if builder.platform_has_fast_fp16:
-        builder.fp16_mode = True
+    def allocate_buffers(self):
+        inputs = []
+        outputs = []
+        bindings = []
+        stream = cuda.Stream()
+        for binding in self.engine:
+            size = trt.volume(self.engine.get_binding_shape(binding)) * self.engine.max_batch_size
+            dtype = trt.nptype(self.engine.get_binding_dtype(binding))
+            # Allocate host and device buffers
+            host_mem = cuda.pagelocked_empty(size, dtype)
+            device_mem = cuda.mem_alloc(host_mem.nbytes)
+            # Append the device buffer to device bindings.
+            bindings.append(int(device_mem))
+            # Append to the appropriate list.
+            if self.engine.binding_is_input(binding):
+                inputs.append(HostDeviceMem(host_mem, device_mem))
+            else:
+                outputs.append(HostDeviceMem(host_mem, device_mem))
+        return inputs, outputs, bindings, stream
 
-    # parse ONNX
-    with open(onnx_file_path, 'rb') as model:
-        print('Beginning ONNX file parsing')
-        parser.parse(model.read())
-    print('Completed parsing of ONNX file')
+    def do_inference_trt(self, bindings, inputs, outputs, stream):
+        # Transfer input data to the GPU.
+        [cuda.memcpy_htod_async(inp.device, inp.host, stream) for inp in inputs]
+        # Run inference.
+        self.context.execute_async_v2(bindings=bindings, stream_handle=stream.handle)
+        # Transfer predictions back from the GPU.
+        [cuda.memcpy_dtoh_async(out.host, out.device, stream) for out in outputs]
+        # Synchronize the stream
+        stream.synchronize()
+        # Return only the host outputs.
+        return [out.host for out in outputs]
+    
+    def pre_process(self,input):
+        pass
 
-    # generate TensorRT engine optimized for the target platform
-    print('Building an engine...')
-    engine = builder.build_cuda_engine(network)
-    save_trt_engine(engine,'scratch.trt')
-    context = engine.create_execution_context()
-    #print("Completed creating Engine")
-    return engine, context
+    def post_process(self, output):
+        pass
 
-def allocate_buffers(engine):
-    inputs = []
-    outputs = []
-    bindings = []
-    stream = cuda.Stream()
-    for binding in engine:
-        size = trt.volume(engine.get_binding_shape(binding)) * engine.max_batch_size
-        dtype = trt.nptype(engine.get_binding_dtype(binding))
-        # Allocate host and device buffers
-        host_mem = cuda.pagelocked_empty(size, dtype)
-        device_mem = cuda.mem_alloc(host_mem.nbytes)
-        # Append the device buffer to device bindings.
-        bindings.append(int(device_mem))
-        # Append to the appropriate list.
-        if engine.binding_is_input(binding):
-            inputs.append(HostDeviceMem(host_mem, device_mem))
-        else:
-            outputs.append(HostDeviceMem(host_mem, device_mem))
-    return inputs, outputs, bindings, stream
+    @staticmethod
+    def _make_grid(nx=20, ny=20):
+        yv, xv = torch.meshgrid([torch.arange(ny), torch.arange(nx)])
+        return torch.stack((xv, yv), 2).view((1, 1, ny, nx, 2)).float()
 
-def do_inference_v2(context, bindings, inputs, outputs, stream):
-    # Transfer input data to the GPU.
-    [cuda.memcpy_htod_async(inp.device, inp.host, stream) for inp in inputs]
-    # Run inference.
-    context.execute_async_v2(bindings=bindings, stream_handle=stream.handle)
-    # Transfer predictions back from the GPU.
-    [cuda.memcpy_dtoh_async(out.host, out.device, stream) for out in outputs]
-    # Synchronize the stream
-    stream.synchronize()
-    # Return only the host outputs.
-    return [out.host for out in outputs]
+    def inference(self,img_paths):
+        dataset = LoadImagesBatch(img_paths,img_size=896)
+
+        batch = []
+        shapes = []
+        paths = []
+        for path, img, im0s, shape in dataset:
+            #img = torch.from_numpy(img).to(self.device)
+            #img = img.half() if self.half else img.float()  # uint8 to fp16/32
+            img = torch.from_numpy(img).float()
+            img /= 255.0  # 0 - 255 to 0.0 - 1.0
+            
+            if img.ndimension() == 3:
+                img = img.unsqueeze(0)
+            
+            batch.append(img)
+            shapes.append(shape)
+            paths.append(path)
+        self.batch_size = len(paths)
+        batch = torch.cat(batch)
+        batch = np.array(batch,dtype=np.float32,order='C')
+
+        print('batch shape :',batch.shape)
+
+        ## inference tensorRT
+        inputs, outputs, bindings, stream = self.allocate_buffers()
+        inputs[0].host = batch
+        trt_outputs = self.do_inference_trt(bindings=bindings, inputs=inputs, outputs=outputs, stream=stream)
+
+        print('done : ',len(trt_outputs) == self.num_detector_layer)
+        raw_output = [] 
+        for i in range(self.num_detector_layer):
+            feature_size = math.sqrt(len(trt_outputs[i])/self.batch_size/4/self.no)
+            feature_size = int(feature_size)
+            y = torch.Tensor(trt_outputs[i]).reshape(self.batch_size,4,feature_size,feature_size,self.no)
+
+            if self.grid[i].shape[2:4] != y.shape[2:4]:
+                self.grid[i] = self._make_grid(feature_size,feature_size)
+            
+            y = y.sigmoid()
+            y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
+            y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
+            raw_output.append(y.view(self.batch_size, -1, self.no))
+        
+        raw_output = torch.cat(raw_output,1)
+
+        raw_output = non_max_suppression(raw_output, self.conf_score, self.iou_thres, agnostic=True)
+
+        output = OrderedDict()
+        for i, det in enumerate(raw_output):  # detections per image
+            out_boxes = OrderedDict()
+            boxes = []
+            labels = []
+            scores = []
+            if det is not None and len(det):
+                # remove batch padding and rescale each image to its original size
+                det[:, :4] = scale_coords(batch.shape[2:], det[:, :4], shapes[i][0],shapes[i][1]).round()
+               
+                # convert tensor to list and scalar
+                for *xyxy, conf, cls in det:
+                    rec = torch.tensor(xyxy).view(1,4).view(-1).int().tolist()
+                    conf = (conf).view(-1).detach().tolist()[0]
+                    h,w = shapes[i][0]
+                    boxes.append(rec)
+                    labels.append(self.label)
+                    scores.append(conf)
+
+            out_boxes['boxes'] = boxes
+            out_boxes['labels'] = labels
+            out_boxes['scores'] = scores   
+            output[paths[i]] = out_boxes
+
+        return output
 
 def main():
-    # initialize TensorRT engine and parse ONNX model
-    #engine, context = build_engine(ONNX_FILE_PATH)
-    engine, context = load_trt_engine('scratch.trt')
     
-    inputs, outputs, bindings, stream = allocate_buffers(engine)
+    paths = ['scratch_yolo/valid/https:__s3.amazonaws.com_mc-imt_vehicle_2020D3862_detail_damage2_51519_medium_A902874A-D6F3-45F2-994F-98A756E21547.jpeg']
+    engine_file_path = 'scratch.trt'
+    model_data_file_path = 'scratch_db'
+    model = TensorRT_model(engine_file_path,model_data_file_path,0.3,0.3,'scratch')
+    out = model.inference(paths)
 
-    print('input : ',inputs)
-    print(len(outputs[0].host))
+    img = cv2.imread(paths[0])
+    for box in out[paths[0]]['boxes']:
+        print(box)
+        img = cv2.rectangle(img,(box[0],box[1]),(box[2],box[3]),(255, 0, 0),1)
 
-
-    # preprocess input data
-    #host_input = np.array(preprocess_image("turkish_coffee.jpg").numpy(), dtype=np.float32, order='C')
-    image = cv2.imread('scratch_yolo/valid/https:__s3.amazonaws.com_mc-ai_dataset_india_20190312_1_IMG_20170417_101024.jpg')
-    image = cv2.resize(image, (896,896), interpolation = cv2.INTER_AREA)
-    image = image / 255.0
-    image = image[:, :, ::-1].transpose(2, 0, 1)    
-    image = np.expand_dims(image, axis=0)
-    image = np.array(image,dtype=np.float32,order='C')
-    print(image.shape)
-    inputs[0].host = image
-
-    trt_outputs = do_inference_v2(context, bindings=bindings, inputs=inputs, outputs=outputs, stream=stream)
-    print('done : ',len(trt_outputs))
-
-    # postprocess results
-    #print(host_output)
-    #output_data = torch.Tensor(host_output).reshape(engine.max_batch_size, output_shape[0])
-
+    cv2.imwrite('test.png',img)
 
 if __name__ == '__main__':
     main()
