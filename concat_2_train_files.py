1a98d3dfe8cf7ca055d05cb64f634b503146cf30
thangnx183 <nxthang881972@gmail.com>
2023-10-05 03:14:12 +0000

    modify concat train files

--- concat_2_train_files.py
+++ concat_2_train_files.py
@@ -1,19 +1,29 @@
 from pathlib import Path 
 import argparse
+import os
 
-def concat_files(file1_path,file2_path,output_path):
-    with open(file1_path) as f :
+def concat_files(files,output_path):
+    des = output_path.replace('.txt','') 
+    Path(des).mkdir(parents=True,exist_ok=True)
+    
+    with open(files[0]) as f :
         origin_data = [line for line in f]
+        os.system('cp '+files[0].replace('.txt','')+ '/* '+des)
+        
 
-    with open(file2_path) as f :
-        merimen_data = [line for line in f]
+    for file in files[1:]:
+        with open(file) as f :
+            merimen_data = [line for line in f]
 
-    addition = [line for line in merimen_data if line not in origin_data]
+        addition = [line for line in merimen_data if line not in origin_data]
+        
+        print('duration :',len(merimen_data)-len(addition))
+        print('addition :',len(addition))
+        
+        origin_data.extend(addition)
+        os.system('cp '+file.replace('.txt','')+ '/* '+des)
     
-    print('duration :',len(merimen_data)-len(addition))
-    print('addition :',len(addition))
-    
-    origin_data.extend(addition)
+    print(files[0].replace('.txt',''),files[0])
 
     with open(output_path,'w') as f:
         for line in origin_data:
@@ -21,13 +31,14 @@ def concat_files(file1_path,file2_path,output_path):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('first_file',type=str,help='path to first file')
-    parser.add_argument('second_file',type=str,help='path to second file')
-    parser.add_argument('output_file',type=str,help='path to output file')
+    parser.add_argument('--files',nargs='+',type=str,help='path to file')
+    # parser.add_argument('second_file',type=str,help='path to second file')
+    parser.add_argument('--output_file',type=str,help='path to output file')
 
     args = parser.parse_args()
+    print(args)
 
-    concat_files(args.first_file, args.second_file, args.output_file)
+    concat_files(args.files, args.output_file)
 
 if __name__ == '__main__':
     main()
