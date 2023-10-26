a9ba73e084065fa2c4b9b5d9b12b1d58d0e25b5b
thangnx183 <nxthang881972@gmail.com>
2023-10-26 10:12:32 +0000

    refactor remove_wrong

--- remove_wrong.py
+++ remove_wrong.py
@@ -1,27 +1,47 @@
 from pathlib import Path 
 import random
+import argparse
 
-wrong_path  = 'coco_stuff_yolo/new_scratch/gen_train_crop.txt'
-origin_path = 'coco_stuff_yolo/new_scratch/merge_train.txt'
 
-wrong = []
-with open(wrong_path) as f:
-    for line in f.readlines():
-        wrong.append(Path(line).stem)
+# wrong_path  = 'coco_stuff_yolo/new_scratch/gen_train_crop.txt'
+# origin_path = 'coco_stuff_yolo/new_scratch/merge_train.txt'
 
-print(wrong)
-# wrong = random.sample(wrong,int(0.5*len(wrong)))
+def remove(origin_path,wrong_path):
+    wrong = []
+    with open(wrong_path) as f:
+        for line in f.readlines():
+            wrong.append(Path(line).stem)
 
-data = []
-with open(origin_path,'r') as f:
-    for line in f.readlines():
-        data.append(line)
+    print(wrong)
+    # wrong = random.sample(wrong,int(0.5*len(wrong)))
 
-new_data = [i for i in data if Path(i).stem not in wrong]
+    data = []
+    with open(origin_path,'r') as f:
+        for line in f.readlines():
+            data.append(line)
 
-# print(data[:3])
-print(len(data),len(new_data))
+    new_data = [i for i in data if Path(i).stem not in wrong]
 
-with open(origin_path,'w') as f:
-    for line in new_data:
-        f.write(line)
+    # print(data[:3])
+    print(len(data),len(new_data))
+
+    with open(origin_path,'w') as f:
+        for line in new_data:
+            f.write(line)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('label_path',type=str,help='path to label file')
+    parser.add_argument('wrong_path',type=str,help='path to wrong files need to remove')
+    
+    args = parser.parse_args()
+    #print('check : ',args)
+    origin_path = args.label_path
+    wrong_path = args.wrong_path
+
+    remove(origin_path,wrong_path)
+
+
+
+if __name__ == '__main__':
+    main()
